5dead117b322fd301cd7b4d9c14628fa2caa521d
Manel Eljishi <manueljishi@gmail.com>
2022-10-20 19:39:18 +0200

    new env and mongo in clodu

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeeklogModule } from './weeklog/weeklog.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     WeeklogModule,
-    MongooseModule.forRoot('mongodb://localhost/powerlog'),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@mongotest.w3q4g.mongodb.net/?retryWrites=true&w=majority`,
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
