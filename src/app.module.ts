09a2d613165a03b4d41cc0164a1f4443c579ca66
manueljishi <manueljishi@gmail.com>
2023-02-16 21:56:25 +0100

    release v2 and remove files controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { RoutineModule } from './routine/routine.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { TrainerDataModule } from './trainer_data/trainer_data.module';
-import { FilesModule } from './files/files.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { RoutineModule } from "./routine/routine.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
+import { TrainerDataModule } from "./trainer_data/trainer_data.module";
+//import { FilesModule } from './files/files.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: ".env",
       expandVariables: true,
     }),
     RoutineModule,
     MongooseModule.forRoot(process.env.MONGODB_URI),
     TrainerDataModule,
-    FilesModule,
+    //FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
