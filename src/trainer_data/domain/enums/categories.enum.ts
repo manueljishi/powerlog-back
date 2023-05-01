b0600beaf86f0efb81b188d2dfbded556d1091c5
Manel <manueljishi@gmail.com>
2023-05-01 17:17:35 +0200

    correct enum typo

--- src/trainer_data/domain/enums/categories.enum.ts
+++ src/trainer_data/domain/enums/categories.enum.ts
@@ -1,10 +1,10 @@
-export enum ExerciseCategories{
-    squat= "squat",
+export enum ExerciseCategories {
+    squat = "squat",
     bench_press = "bench_press",
     deadlift = "deadlift",
     knee_dominant = "knee_dominant",
     hip_dominant = "hip_dominant",
     push = "push",
     pull = "pull",
-    complementary = "comlpementary",
+    complementary = "complementary",
 }
